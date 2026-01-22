9fc4c47f58a9bd4c0a4bb16a3308a88ab6bf33be
jonia89 <jonianttila89@hotmail.com>
2026-01-22 18:13:48 +0200

    #17 siirrettiin varaukset huoneiden alle

--- src/models/Reservation.ts
+++ src/models/Reservation.ts
@@ -1,6 +1,5 @@
 export interface Reservation {
   id: number;
-  roomId: number;
   startTime: Date;
   endTime: Date;
 }
--- src/models/Room.ts
+++ src/models/Room.ts
@@ -1,5 +1,8 @@
+import { Reservation } from "./Reservation";
+
 export interface Room {
   id: number;
   name: string;
   capacity: number;
+  roomReservations: Reservation[];
 }
